66333d4fa222596a64595f3e574c90383a89407e
Sheetala Soni <52600600+sp-soni@users.noreply.github.com>
2022-07-15 13:48:15 +0530

    Update spitech_crud.sql

--- sql/spitech_crud.sql
+++ sql/spitech_crud.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jul 04, 2022 at 07:34 AM
--- Server version: 8.0.21
--- PHP Version: 7.4.9
+-- Host: 127.0.0.1
+-- Generation Time: Jul 15, 2022 at 10:17 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,29 +27,84 @@ SET time_zone = "+00:00";
 -- Table structure for table `project`
 --
 
-DROP TABLE IF EXISTS `project`;
-CREATE TABLE IF NOT EXISTS `project` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `db_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `platform` enum('codeigniter-3.x','laravel-8.x') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'laravel-8.x',
-  `base_model_suffix` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `controller_prefix` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `route_prefix` text COLLATE utf8mb4_general_ci NOT NULL,
-  `project_name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `controller_path` text COLLATE utf8mb4_general_ci NOT NULL,
-  `model_path` text COLLATE utf8mb4_general_ci NOT NULL,
-  `view_path` text COLLATE utf8mb4_general_ci NOT NULL,
-  `route_path` text COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL,
+  `project_name` varchar(100) NOT NULL,
+  `db_name` varchar(100) NOT NULL,
+  `platform` enum('codeigniter-3.x','laravel-8.x') NOT NULL DEFAULT 'laravel-8.x',
+  `root_path` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `project`
 --
 
-INSERT INTO `project` (`id`, `db_name`, `platform`, `base_model_suffix`, `controller_prefix`, `route_prefix`, `project_name`, `controller_path`, `model_path`, `view_path`, `route_path`) VALUES
-(1, 'u172594077_demoecom', 'laravel-8.x', 'Base', '', '', 'Product Ecom', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\Modules\\Admin\\Http\\Controllers/', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\app\\Models/', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\Modules\\Admin\\Resources\\views/', ''),
-(2, 'product_billing_ci', 'codeigniter-3.x', 'Base', 'Admin_', 'admin_', 'Product Billing CI', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\Modules\\Admin\\Http\\Controllers/', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\app\\Models/', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\Modules\\Admin\\Resources\\views/', '');
+INSERT INTO `project` (`id`, `project_name`, `db_name`, `platform`, `root_path`) VALUES
+(1, 'Product-Ecom', 'u103431999_ecom_test', 'laravel-8.x', ''),
+(2, 'Product-Billing-CI', 'product_billing_ci', 'codeigniter-3.x', ''),
+(3, 'Product-Broker-CI', 'u172594077_demobroker_ci', 'codeigniter-3.x', ''),
+(4, 'Product-HRMS', 'u172594077_demohrms', 'laravel-8.x', 'C:\\xampp\\htdocs\\practice\\products\\product_hrms');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_module`
+--
+
+CREATE TABLE `project_module` (
+  `id` int(11) NOT NULL,
+  `module` varchar(50) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `controller_parent_class` varchar(250) NOT NULL,
+  `base_model_suffix` varchar(50) NOT NULL,
+  `controller_path` text NOT NULL,
+  `model_path` text NOT NULL,
+  `view_path` text NOT NULL,
+  `route_path` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `project_module`
+--
+
+INSERT INTO `project_module` (`id`, `module`, `project_id`, `controller_parent_class`, `base_model_suffix`, `controller_path`, `model_path`, `view_path`, `route_path`) VALUES
+(1, 'Admin', 1, 'App\\CustomComponents\\AdminAbstractController', 'Base', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\Modules\\Admin\\Http\\Controllers', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\app\\Models', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\Modules\\Admin\\Resources\\views', ''),
+(2, 'Vendor', 1, 'App\\CustomComponents\\VendorAbstractController', 'Base', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\Modules\\Vendor\\Http\\Controllers', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\app\\Models', 'D:\\wamp64\\www\\spsoni\\products\\product_ecom\\Modules\\Vendor\\Resources\\views', ''),
+(3, 'Admin', 2, 'NA', 'Base', 'C:\\xampp\\htdocs\\practice\\products\\product_billing_ci\\app\\modules\\admin\\controllers', 'C:\\xampp\\htdocs\\practice\\products\\product_billing_ci\\app\\models', 'C:\\xampp\\htdocs\\practice\\products\\product_billing_ci\\app\\modules\\admin\\views', ''),
+(4, 'Admin', 3, 'NA', 'Base', 'D:\\wamp64\\www\\spsoni\\products\\product_broker_ci\\app\\modules\\admin\\controllers', 'D:\\wamp64\\www\\spsoni\\products\\product_broker_ci\\app\\models', 'D:\\wamp64\\www\\spsoni\\products\\product_broker_ci\\app\\modules\\admin\\views', ''),
+(5, 'Admin', 4, 'App\\CustomComponents\\AdminAbstractController', 'Base', 'Modules\\Admin\\Http\\Controllers', 'app\\Models', 'Modules\\Admin\\Resources\\views', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `project_module`
+--
+ALTER TABLE `project_module`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `project_module`
+--
+ALTER TABLE `project_module`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
