6923b69fa9bc988c3809c4432c1c1270fe050a86
SPSONI <spitechsoft@gmail.com>
2022-08-31 13:11:07 +0530

    Update spitech_crud.sql

--- sql/spitech_crud.sql
+++ sql/spitech_crud.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 30, 2022 at 11:43 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 31, 2022 at 07:22 AM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -25,7 +25,8 @@ DELIMITER $$
 --
 -- Procedures
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `add_column_if_not_exists` (IN `table_name_IN` VARCHAR(100), IN `field_name_IN` VARCHAR(100), IN `field_definition_IN` VARCHAR(100))   BEGIN
+DROP PROCEDURE IF EXISTS `add_column_if_not_exists`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_column_if_not_exists` (IN `table_name_IN` VARCHAR(100), IN `field_name_IN` VARCHAR(100), IN `field_definition_IN` VARCHAR(100))  BEGIN
 
     SET @isFieldThere = column_exists(table_name_IN, field_name_IN);
     IF (@isFieldThere = 0) THEN
@@ -43,7 +44,8 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `add_column_if_not_exists` (IN `tabl
 
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `drop_column_if_exists` (`tname` VARCHAR(64), `cname` VARCHAR(64))   BEGIN
+DROP PROCEDURE IF EXISTS `drop_column_if_exists`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `drop_column_if_exists` (`tname` VARCHAR(64), `cname` VARCHAR(64))  BEGIN
     IF column_exists(tname, cname)
     THEN
       SET @drop_column_if_exists = CONCAT('ALTER TABLE `', tname, '` DROP COLUMN `', cname, '`');
@@ -55,7 +57,8 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `drop_column_if_exists` (`tname` VAR
 --
 -- Functions
 --
-CREATE DEFINER=`root`@`localhost` FUNCTION `column_exists` (`table_name_IN` VARCHAR(100), `field_name_IN` VARCHAR(100)) RETURNS INT(11)  RETURN (
+DROP FUNCTION IF EXISTS `column_exists`$$
+CREATE DEFINER=`root`@`localhost` FUNCTION `column_exists` (`table_name_IN` VARCHAR(100), `field_name_IN` VARCHAR(100)) RETURNS INT(11) RETURN (
     SELECT COUNT(COLUMN_NAME) 
     FROM INFORMATION_SCHEMA.columns 
     WHERE TABLE_SCHEMA = DATABASE() 
@@ -71,13 +74,15 @@ DELIMITER ;
 -- Table structure for table `project`
 --
 
-CREATE TABLE `project` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `project`;
+CREATE TABLE IF NOT EXISTS `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `project_name` varchar(100) NOT NULL,
   `db_name` varchar(100) NOT NULL,
   `platform` enum('codeigniter-3.x','laravel-8.x') NOT NULL DEFAULT 'laravel-8.x',
-  `root_path` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+  `root_path` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `project`
@@ -88,7 +93,8 @@ INSERT INTO `project` (`id`, `project_name`, `db_name`, `platform`, `root_path`)
 (2, 'Product-Billing-CI', 'product_billing_ci', 'codeigniter-3.x', 'C:\\xampp\\htdocs\\www\\products\\product_billing_ci'),
 (3, 'Product-Broker-CI', 'u172594077_demobroker_ci', 'codeigniter-3.x', 'C:\\xampp\\htdocs\\www\\products\\product_broker_ci'),
 (4, 'Product-HRMS', 'u172594077_demohrms', 'laravel-8.x', 'C:\\xampp\\htdocs\\www\\products\\product_hrms'),
-(5, 'SPS-Accounting', 'u103431999_spsoni_account', 'codeigniter-3.x', 'C:\\xampp\\htdocs\\www\\products\\sps');
+(5, 'SPS-Accounting', 'u103431999_spsoni_account', 'codeigniter-3.x', 'C:\\xampp\\htdocs\\www\\products\\sps'),
+(6, 'CityWalaDriver', 'u103431999_citywaladriver', 'codeigniter-3.x', 'C:\\wamp64\\www\\html\\citywaladriver.com');
 
 -- --------------------------------------------------------
 
@@ -96,8 +102,9 @@ INSERT INTO `project` (`id`, `project_name`, `db_name`, `platform`, `root_path`)
 -- Table structure for table `project_module`
 --
 
-CREATE TABLE `project_module` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `project_module`;
+CREATE TABLE IF NOT EXISTS `project_module` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `module` varchar(50) NOT NULL,
   `project_id` int(11) NOT NULL,
   `controller_parent_class` varchar(250) NOT NULL,
@@ -105,8 +112,9 @@ CREATE TABLE `project_module` (
   `controller_path` text NOT NULL,
   `model_path` text NOT NULL,
   `view_path` text NOT NULL,
-  `route_path` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+  `route_path` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `project_module`
@@ -118,39 +126,8 @@ INSERT INTO `project_module` (`id`, `module`, `project_id`, `controller_parent_c
 (3, 'Admin', 2, 'NA', 'Base', 'C:\\xampp\\htdocs\\practice\\products\\product_billing_ci\\app\\modules\\admin\\controllers', 'C:\\xampp\\htdocs\\practice\\products\\product_billing_ci\\app\\models', 'C:\\xampp\\htdocs\\practice\\products\\product_billing_ci\\app\\modules\\admin\\views', ''),
 (4, 'Admin', 3, 'NA', 'Base', 'D:\\wamp64\\www\\spsoni\\products\\product_broker_ci\\app\\modules\\admin\\controllers', 'D:\\wamp64\\www\\spsoni\\products\\product_broker_ci\\app\\models', 'D:\\wamp64\\www\\spsoni\\products\\product_broker_ci\\app\\modules\\admin\\views', ''),
 (5, 'Admin', 4, 'App\\CustomComponents\\AdminAbstractController', 'Base', 'Modules\\Admin\\Http\\Controllers', 'app\\Models', 'Modules\\Admin\\Resources\\views', ''),
-(6, 'Admin', 5, 'NA', 'Base', 'app\\modules\\admin\\controllers', 'app\\models', 'app\\modules\\admin\\views', '');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `project`
---
-ALTER TABLE `project`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `project_module`
---
-ALTER TABLE `project_module`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `project`
---
-ALTER TABLE `project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `project_module`
---
-ALTER TABLE `project_module`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+(6, 'Admin', 5, 'NA', 'Base', 'app\\modules\\admin\\controllers', 'app\\models', 'app\\modules\\admin\\views', ''),
+(7, 'Admin', 6, 'NA', 'Base', 'app\\modules\\admin\\controllers', 'app\\models', 'app\\modules\\admin\\views', '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
